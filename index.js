ec416ad66ff468bf74cabe634f80a2ae7e51c783
MIGUEL PUCHE ALONSO <miguel@MIGUELs-MacBook-Pro.local>
2019-06-26 14:53:16 +0100

    last_big

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const n = 11 - 1;
 
 function create_init_board(){
     position = [
-        [0, 0, 0, 8, 8, 8, 8, 8, 0, 0, 0],
-        [0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [8, 0, 0, 0, 0, 1, 0, 0, 0, 0, 8],
         [8, 0, 0, 0, 1, 1, 1, 0, 0, 0, 8],
-        [8, 8, 0, 1, 1, 2, 1, 1, 0, 8, 8],
+        [8, 0, 0, 0, 0, 2, 0, 0, 0, 0, 8],
         [8, 0, 0, 0, 1, 1, 1, 0, 0, 0, 8],
         [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
-        [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
-        [0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0],
-        [0, 0, 0, 8, 8, 8, 8, 8, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]
     board = {
         position: position,
         score: 0,
-        children:[]
+        children:[],
+        parent: undefined,
+        level: 0,
+        previous_move: undefined,
+        best_move: undefined,
     }
     return board;
 }
@@ -30,35 +34,46 @@ function display_board(board){
     console.log()
 }
 
-function apply_move (board, fr_row, fr_col, to_row, to_col){
-    const new_board = JSON.parse(JSON.stringify(board));
-    const fr_value = board.position[fr_row][fr_col];
-    const to_value = board.position[to_row][to_row];
+function apply_move (board, move){
+    //const new_board = JSON.parse(JSON.stringify(board));
+    const arr = JSON.parse(JSON.stringify(board.position));
+    let new_board = {position: arr};
+    const fr_row = move.fr[0];
+    const fr_col = move.fr[1];
+    const to_row = move.to[0];
+    const to_col = move.to[1];
+    const fr_val = board.position[fr_row][fr_col];
+    const to_val = board.position[to_row][to_row];
 
-    if (to_value !==0) throw error;
+    //if (to_val !==0) throw error;
     if (fr_row < 0 || n < fr_row) throw error;
     if (fr_col < 0 || n < fr_col) throw error;
     if (to_row < 0 || n < fr_row) throw error;
     if (to_col < 0 || n < to_col) throw error;
 
-    new_board.position[fr_row][fr_col] = to_value;
-    new_board.position[to_row][to_col] = fr_value;
+    new_board.position[fr_row][fr_col] = to_val;
+    new_board.position[to_row][to_col] = fr_val;
+    new_board.level = board.level + 1;
+    new_board.children = [];
     return new_board;
 }
 
 function evaluate_board (board){
     const arr = board.position;
     let score = 0;
-    if (board.position[0][0] === 2) score = 1000;
-    if (board.position[0][n] === 2) score = 1000;
-    if (board.position[n][0] === 2) score = 1000;
-    if (board.position[n][n] === 2) score = 1000;
+    
+    if (board.position[0][0] === 2) return 1000;
+    if (board.position[0][n] === 2) return 1000;
+    if (board.position[n][0] === 2) return 1000;
+    if (board.position[n][n] === 2) return 1000;
+    
     arr.map(row => {
         row.map(value => {
             if (value === 1) score += 1;
             if (value === 8) score -= 0.42325;
         });
     });
+
     score = Math.round(score);
     board.score = score;
     return Math.round(score);
@@ -71,23 +86,89 @@ function generate_possible_moves(board, cell){
 
     //up
     to_row = fr_row -1;
-    while (board.position[to_row][to_col] !== 0 || board.position[to_row][to_col] > -1) {
+    to_col = fr_col;
+    while (to_row > -1 && board.position[to_row][to_col] === 0) {
         possible_moves.push({fr:[fr_row, fr_col], to:[to_row, to_col]});
         to_row -= 1;
     }
 
     //right
+    to_row = fr_row;
+    to_col = fr_col + 1;
+    while (to_col < n +1 && board.position[to_row][to_col] === 0) {
+        possible_moves.push({fr:[fr_row, fr_col], to:[to_row, to_col]});
+        to_col += 1;
+    }
     
     //down
+    to_row = fr_row + 1;
+    to_col = fr_col;
+    while (to_row < n + 1 && board.position[to_row][to_col] === 0) {
+        possible_moves.push({fr:[fr_row, fr_col], to:[to_row, to_col]});
+        to_row += 1;
+    }
     
     //left
-    return [];
+    to_row = fr_row;
+    to_col = fr_col - 1;
+    while (to_col < n +1 && board.position[to_row][to_col] === 0) {
+        possible_moves.push({fr:[fr_row, fr_col], to:[to_row, to_col]});
+        to_col -= 1;
+    }
+
+
+    return possible_moves;
 }
+
+function generate_board_possible_moves(board) {
+    const arr = board.position;
+    let moves= [];
+    arr.map((row, i)=> {
+        row.map((val, j) => {
+            if (val === 8 && board.level % 2 === 0){
+                moves = moves.concat(generate_possible_moves(board, [i, j]));
+            }
+            if ((val === 1 || val === 2) && board.level % 2 === 1){
+                moves = moves.concat(generate_possible_moves(board, [i, j]));
+            }
+        })
+    })
+    return moves;
+}
+
+function generate_children(board){
+    moves = generate_board_possible_moves(board, 'black');
+    moves.map(move => {
+        board_child = apply_move(board, move);
+        board.children.push (board_child);
+    })
+}
+
+function generate_children_rec (board, max_level) {
+    if (board.level > max_level - 1) return;
+    generate_children(board);
+    board.children.map(board => generate_children_rec (board, max_level))
+}
+
 
 /// 
 board = create_init_board();
-display_board(board);
+//cdisplay_board(board);
 //const new_board = apply_move (board, 0, 3, 'left');
 //display_board(new_board);
-const score = evaluate_board(board);
-console.log(score);cl
+//const score = evaluate_board(board);
+//console.log(score);
+//moves = generate_board_possible_moves(board, 'black');
+generate_children_rec(board, 3);
+//console.log(board);
+//console.log(board.children[0])
+//move = moves[0];
+//console.log(move);
+//new_board = apply_move(board, move);
+//display_board(new_board);
+display_board(board);
+display_board(board.children[0]);
+display_board(board.children[0].children[0]);
+display_board(board.children[0].children[0].children[0]);
+//display_board(board.children[0].children[0].children[0].children[0]);
+console.log('end')
